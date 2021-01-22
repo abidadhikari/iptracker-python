c81de46d948a56153efe82403835bed0a6dfc91d
Abid Adhikari <abidadhikari@gmail.com>
2021-01-22 17:17:36 +0545

    updated

--- iplocation.py
+++ iplocation.py
@@ -1,5 +1,5 @@
 import pygeoip
 gip=pygeoip.GeoIP("GeoLiteCity.dat")
-res=gip.record_by_addr('27.34.16.159')
+res=gip.record_by_addr('')  #ip address goes here
 for key,value in res.items():
     print('%s : %s' %(key,value))
